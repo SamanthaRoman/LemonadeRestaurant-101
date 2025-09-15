509048b84833b072a5479f171156596ba5151c38
Samantha Roman <samantharaeroman@gmail.com>
2025-09-15 17:35:25 -0400

    Initial Commit

---
+++ LemonadeRestaurant/LemonadeRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  LemonadeRestaurantApp.swift
+//  LemonadeRestaurant
+//
+//  Created by Samantha Roman on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct LemonadeRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LemonadeRestaurantTests/LemonadeRestaurantTests.swift
@@ -0,0 +1,17 @@
+//
+//  LemonadeRestaurantTests.swift
+//  LemonadeRestaurantTests
+//
+//  Created by Samantha Roman on 9/15/25.
+//
+
+import Testing
+@testable import LemonadeRestaurant
+
+struct LemonadeRestaurantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
